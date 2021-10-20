ded40c8174cdecb0481539516c906bf5964e8be6
geoffreyChet <73818181+geoffreyChet@users.noreply.github.com>
2021-10-20 15:39:13 +0200

    New version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dbstream",
-    version="0.1.7",
+    version="0.1.8",
     author="Dacker",
     author_email="hello@dacker.co",
     description="A meta package to be connected to several databases",
