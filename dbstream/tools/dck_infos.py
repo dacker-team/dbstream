ebf98b416fbbe3f55b7eec63ef88f5efb6810c38
Geoffrey Chetcuti <gchetcuti-ext@moethennessy.com>
2021-10-29 11:50:16 +0200

    Batch id for send data

--- dbstream/tools/dck_infos.py
+++ dbstream/tools/dck_infos.py
@@ -2,7 +2,7 @@ import datetime
 import uuid
 
 
-def generate_dck_info(row_data):
+def generate_dck_info(row_data, batch_id=None):
     result = []
     _now = datetime.datetime.now()
     for row in row_data:
@@ -10,5 +10,7 @@ def generate_dck_info(row_data):
             row['__dck_id___'] = str(uuid.uuid4())
         if not row.get('__dck_inserted_at___'):
             row['__dck_inserted_at___'] = _now
+        if batch_id and not row.get('__dck_batch_id___'):
+            row['__dck_batch_id___'] = batch_id
         result.append(row)
     return result
